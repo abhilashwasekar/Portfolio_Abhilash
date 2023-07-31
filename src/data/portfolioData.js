06e5f81e67819775069a86113ef450cc0ba41425
rameezqazi23 <rameez.qazi23@gmail.com>
2023-08-01 02:28:48 +0500

    re-revert changes try to resolve issues

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,107 +1,107 @@
-// import porfolioImg01 from '../asset/projects/portfolio-01.png'
-// import porfolioImg02 from '../asset/projects/portfolio-02.png'
-// import porfolioImg03 from '../asset/projects/portfolio-03.png'
-// import porfolioImg04 from '../asset/projects/portfolio-04.png'
-// import porfolioImg05 from '../asset/projects/portfolio-05.png'
-// import porfolioImg06 from '../asset/projects/portfolio-06.png'
+import porfolioImg01 from '../asset/projects/portfolio-01.png'
+import porfolioImg02 from '../asset/projects/portfolio-02.png'
+import porfolioImg03 from '../asset/projects/portfolio-03.png'
+import porfolioImg04 from '../asset/projects/portfolio-04.png'
+import porfolioImg05 from '../asset/projects/portfolio-05.png'
+import porfolioImg06 from '../asset/projects/portfolio-06.png'
 
 
-// const portfolioData = [
-//     {
-//         id: '01',
-//         imgUrl: porfolioImg01,
-//         title: 'Business',
-//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-//         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-//         siteUrl: '#',
-//     },
+const portfolioData = [
+    {
+        id: '01',
+        imgUrl: porfolioImg01,
+        title: 'Business',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
+        tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
+        siteUrl: '#',
+    },
 
-//     {
-//         id: '02',
-//         imgUrl: porfolioImg02,
-//         title: 'Business',
-//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-//         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-//         siteUrl: '#',
-//     },
+    {
+        id: '02',
+        imgUrl: porfolioImg02,
+        title: 'Business',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
+        tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
+        siteUrl: '#',
+    },
 
-//     {
-//         id: '03',
-//         imgUrl: porfolioImg03,
-//         title: 'Business',
-//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-//         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-//         siteUrl: '#',
-//     },
+    {
+        id: '03',
+        imgUrl: porfolioImg03,
+        title: 'Business',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
+        tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
+        siteUrl: '#',
+    },
 
-//     {
-//         id: '04',
-//         imgUrl: porfolioImg04,
-//         title: 'Business',
-//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-//         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-//         siteUrl: '#',
-//     },
+    {
+        id: '04',
+        imgUrl: porfolioImg04,
+        title: 'Business',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
+        tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
+        siteUrl: '#',
+    },
 
-//     {
-//         id: '05',
-//         imgUrl: porfolioImg05,
-//         title: 'Business',
-//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-//         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-//         siteUrl: '#',
-//     },
+    {
+        id: '05',
+        imgUrl: porfolioImg05,
+        title: 'Business',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
+        tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
+        siteUrl: '#',
+    },
 
-//     {
-//         id: '06',
-//         imgUrl: porfolioImg06,
-//         title: 'Business',
-//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-//         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-//         siteUrl: '#',
-//     },
-//     {
-//         id: '07',
-//         imgUrl: porfolioImg06,
-//         title: 'Business',
-//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-//         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-//         siteUrl: '#',
-//     },
+    {
+        id: '06',
+        imgUrl: porfolioImg06,
+        title: 'Business',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
+        tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
+        siteUrl: '#',
+    },
+    {
+        id: '07',
+        imgUrl: porfolioImg06,
+        title: 'Business',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
+        tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
+        siteUrl: '#',
+    },
 
-//     {
-//         id: '08',
-//         imgUrl: porfolioImg06,
-//         title: 'Business',
-//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-//         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-//         siteUrl: '#',
-//     },
-//     {
-//         id: '09',
-//         imgUrl: porfolioImg06,
-//         title: 'Business',
-//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-//         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-//         siteUrl: '#',
-//     },
-//     {
-//         id: '10',
-//         imgUrl: porfolioImg06,
-//         title: 'Business',
-//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-//         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-//         siteUrl: '#',
-//     },
-//     {
-//         id: '11',
-//         imgUrl: porfolioImg06,
-//         title: 'Business',
-//         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-//         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-//         siteUrl: '#',
-//     },
+    {
+        id: '08',
+        imgUrl: porfolioImg06,
+        title: 'Business',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
+        tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
+        siteUrl: '#',
+    },
+    {
+        id: '09',
+        imgUrl: porfolioImg06,
+        title: 'Business',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
+        tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
+        siteUrl: '#',
+    },
+    {
+        id: '10',
+        imgUrl: porfolioImg06,
+        title: 'Business',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
+        tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
+        siteUrl: '#',
+    },
+    {
+        id: '11',
+        imgUrl: porfolioImg06,
+        title: 'Business',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
+        tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
+        siteUrl: '#',
+    },
 
-// ]
+]
 
-// export default portfolioData;
+export default portfolioData;
