8e69d5adf210ac77a75e0622cbedcf4b6883e708
rameezqazi23 <rameez.qazi23@gmail.com>
2023-09-21 13:04:10 +0500

    add new project and add some minor changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -95,12 +95,12 @@ const Navbar = () => {
             <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
                 <ul>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300 bg-[#171515]'>
-                        <a href="https://github.com/rameezqazi23" target='_blank' className='flex justify-between items-center w-full text-gray-300'>
+                        <a href="https://github.com/rameezqazi23" rel='noreferrer' target='_blank' className='flex justify-between items-center w-full text-gray-300'>
                             Github <FaGithub size={30} />
                         </a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[0px] duration-300 bg-[#0A66C2]'>
-                        <a href="https://www.linkedin.com/in/rameez-qazi-754554160/" target='_blank' className='flex justify-between items-center w-full text-gray-300'>
+                        <a href="https://www.linkedin.com/in/rameez-qazi-754554160/" rel='noreferrer' target='_blank' className='flex justify-between items-center w-full text-gray-300'>
                             LinkedIn <FaLinkedin size={30} />
                         </a>
                     </li>
--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,8 +1,10 @@
-import potfolioImg01 from '../asset/projects/projectImg-01.JPG';
-import potfolioImg02 from '../asset/projects/projectImg-02.JPG';
-import potfolioImg03 from '../asset/projects/projectImg-03.JPG';
-import potfolioImg04 from '../asset/projects/projectImg-04.JPG';
-import potfolioImg05 from '../asset/projects/projectImg-05.JPG';
+import portfolioImg01 from '../asset/projects/projectImg-01.JPG';
+import portfolioImg02 from '../asset/projects/projectImg-02.JPG';
+import portfolioImg03 from '../asset/projects/projectImg-03.JPG';
+import portfolioImg04 from '../asset/projects/projectImg-04.JPG';
+import portfolioImg05 from '../asset/projects/projectImg-05.JPG';
+import portfolioImg06 from '../asset/projects/projectImg-06.JPG';
+
 
 
 
@@ -13,7 +15,7 @@ import potfolioImg05 from '../asset/projects/projectImg-05.JPG';
 const portfolioData = [
     {
         id: '01',
-        imgUrl: potfolioImg01,
+        imgUrl: portfolioImg01,
         title: 'React+Vite Dashboard',
         description: 'Crafted with React and Vite, our cutting-edge dashboard project features dynamic data visualization using Tremor components. Experience real-time insights and seamless user interaction, all powered by the latest web technologies.',
         tech: ["React", "Tailwind css", "Tremor", "Vercel", "Vite", "Hero-Icons"],
@@ -22,7 +24,7 @@ const portfolioData = [
 
     {
         id: '02',
-        imgUrl: potfolioImg02,
+        imgUrl: portfolioImg02,
         title: 'Netflix App',
         description: 'Netflix-inspired project powered by React and Tailwind CSS. Deployed on Vercel, with Firebase for the database and movie information. Elevate your streaming experience with sleek design and efficient data management.',
         tech: ["React", "Tailwind css", "Movie Database", "Firebase", "Vercel"],
@@ -31,7 +33,7 @@ const portfolioData = [
 
     {
         id: '03',
-        imgUrl: potfolioImg03,
+        imgUrl: portfolioImg03,
         title: 'Crypto Base App',
         description: 'Crypto Base App: React and Tailwind CSS harmonize for seamless UI. Vercel hosts it, Coingecko API fuels live data, and Firebase stores favorites. Enjoy React Sparkline, Ant Design finesse, and real-time insights.',
         tech: ["React", "Tailwind css", "React Icons", "Ant Design", "React Sparklines"],
@@ -40,7 +42,7 @@ const portfolioData = [
 
     {
         id: '04',
-        imgUrl: potfolioImg04,
+        imgUrl: portfolioImg04,
         title: 'Travel Landing Page',
         description: 'Travel Landing: Captivating React creation infused with Tailwind CSS and React Icons. Experience wanderlust brought to life, hosted effortlessly on Netlify.',
         tech: ["React", "Tailwind css", "React Icons"],
@@ -49,21 +51,21 @@ const portfolioData = [
 
     {
         id: '05',
-        imgUrl: potfolioImg05,
+        imgUrl: portfolioImg05,
         title: 'Healthcare Website',
         description: 'Explore Health Website: A robust React creation harmonized with Tailwind CSS and enhanced with React Icons. Dive into comprehensive health care facilities showcased elegantly. Seamlessly deployed on Netlify and hosted on Namecheap for an immersive online experience.',
         tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
         siteUrl: 'https://www.ajanihealthcare.com/',
     },
 
-    // {
-    //     id: '06',
-    //     imgUrl: porfolioImg06,
-    //     title: 'Business',
-    //     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis a amet corrupti. Quas id, hic facilis ab nulla aperiam laudantium sint placeat dolorum? Error illo dicta laudantium doloribus consectetur animi.',
-    //     tech: ["React", "Tailwind css", "Node.js", "MongoDB"],
-    //     siteUrl: '#',
-    // },
+    {
+        id: '06',
+        imgUrl: portfolioImg06,
+        title: 'Web3.0 Crowdfunding Web App',
+        description: 'Crafting a cutting-edge Web3 crowdfunding platform akin to Kickstarter. We are leveraging React+Vite for seamless UI, Thirdweb for decentralized magic, Metamask for wallet wizardry, Solidity for rock-solid smart contracts, and Tailwind CSS for a sleek design thats set to revolutionize crowdfunding.',
+        tech: ["React+Vite", "Tailwind css", "Solidity", "MetaMask", "Smart Contract","HardHat", "Vercel"],
+        siteUrl: 'https://web3-thirdweb-seven.vercel.app/',
+    },
     // {
     //     id: '07',
     //     imgUrl: porfolioImg06,
